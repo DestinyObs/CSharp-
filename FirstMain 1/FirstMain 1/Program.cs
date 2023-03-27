703e68d677f0654df39f79260a695859052a8b15
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-03-27 14:45:27 +0100

    Update

--- FirstMain 1/FirstMain 1/Program.cs
+++ FirstMain 1/FirstMain 1/Program.cs
@@ -1,16 +1,66 @@
-﻿namespace FirstMain_1
+﻿using PATA = ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
+using System.Collections;
+using System.Configuration; 
+
+namespace FirstMain_1
 {
+    class Customer
+    {
+        string firstName;
+        string lastName;
+
+        public Customer() 
+            : this("No firstName Provided", "No firstName Provided")
+        { 
+
+        }
+        //contructor - used the initialize your class fields
+        //are called automatically wil you create an instance of a class.... can take parameter
+        public Customer(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        //Method - represent the behavoiur!
+        public void getFullName()
+        {
+            Console.WriteLine($"Full Name = {firstName} {lastName}");
+
+        }
+        //a class can also have destructors to clean up resources your class was holding it its life time.
+        // cannot take paramters and also has a tilled before the class name
+        ~Customer()
+        {
+            //clean up code
+        }
+
+
+
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            /* int Total = 0;
+            Customer C1 = new Customer();
+            C1.getFullName();
+
+            Customer C2 = new Customer("Destiny", "Obs");
+            C2.getFullName();
+            //overloading contructors
+
+            PATA.ClassA.print();
+            PATB.ClassB.print();
+            // namespaces are used to organize your program.... 
+
+             int Total = 0;
              int Product = 0;
 
              Add(10, 20, out Total, out Product);
 
              Console.WriteLine($"sunans = {Total} && Product = {Product}");
-            */
+            
 
             int[] num = new int[7];
             num[0] = 101;
@@ -24,16 +74,22 @@
             //ParamsMethod();
             //ParamsMethod(num);
             ParamsMethod(1, 2, 3, 4, 5);
+            //invoking method paramters are called arguments
+
+            
 
         }
 
-        /*public static void Add(int x, int y, out int sum, out int Product) 
+        public static void Add(int x, int y, out int sum, out int Product) 
         { 
             sum = x + y;
             Product = x * y;
         }
-        */
 
+        //Value paramter
+        //ref paramter
+        //output paramter
+        
         public static void ParamsMethod(params int[] num)
             //params as to be the last
         {
@@ -43,6 +99,33 @@
                 Console.WriteLine(i);
             }
 
+        }
+        
+
+    }
+}
+
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        class ClassA
+        {
+            public static void print()
+            {
+                Console.WriteLine("Yo Yo ");
+            }
+        }
+    }
+
+    namespace TeamB
+    {
+        class ClassB
+        {
+            public static void print()
+            {
+                Console.WriteLine("How are you my friend");
+            }
         }
     }
 }
---
+++ FirstMain 1/ProjectA.TeamB/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectA.TeamB
+{
+    public class ClassA
+    {
+        public static void print()
+        {
+            Console.WriteLine("Team A is my name");
+        }
+    }
+}
