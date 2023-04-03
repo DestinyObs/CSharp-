1fabbc8ef86ff1c8e7652f6ee02638d66d894733
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-04-03 09:46:40 +0100

    New

---
+++ Multiple class inheritance/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Multiple_class_inheritance
+{
+    interface IA
+    {
+        void AMethod();
+    }
+
+    class A : IA
+    {
+        public void AMethod()
+        {
+            Console.WriteLine("A");
+        }
+
+    }
+
+    interface IB
+    {
+        void BMethod();
+    }
+
+    class B : IB
+    {
+        public void BMethod()
+        {
+            Console.WriteLine("B");
+        }
+
+    }
+
+    class AB : IA, IB
+    {
+        A a = new A();
+        B b = new B();
+        public void AMethod()
+        {
+            a.AMethod();
+        }
+
+        public void BMethod()
+        {
+            b.BMethod();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AB aB = new AB();
+            aB.AMethod();
+            aB.BMethod();
+
+        }
+    }
+}
