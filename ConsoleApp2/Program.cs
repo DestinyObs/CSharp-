d3dd03e49f1671d58bf6aa9095cb7389a6f05bfe
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-04-05 11:00:36 +0100

    New

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Dynamic;
+
+namespace ConsoleApp2
+{
+
+    //this is the type and fields 
+    public class Customer
+    {
+        #region Fields
+        private int _id;
+        private string _LName;
+        private string _FName;
+        #endregion
+
+        #region Properties
+        public int ID
+        {
+            //properties is the and method are types memebers 
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string LastName
+        {
+            get { return _LName; }
+            set { _LName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _FName; }
+            set { _FName = value; }
+        }
+        #endregion
+
+        #region Methods
+        public string GetFullName()
+        {
+            return _FName + " " + _LName;
+        }
+        #endregion
+    }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                
+                
+                //difference btw types and type memebers 
+                //organizing codes with regions
+
+                //type memebers have all access modifiers whereas type can have only
+                //2 (internal and public)
+                //regions has you organize your code
+
+            }
+        }
+}
