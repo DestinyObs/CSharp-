cef945be6b9e749e351be52ec47c56bd40ddba92
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-04-05 10:59:27 +0100

    continue

---
+++ Enum Con't/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Enum_Con_t
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Enum = class
+            //enum = is a keyword to create enumerations
+            //trying to convert enum type
+
+            //Type casting and conversion
+
+
+            Gender gender = (Gender)3;
+            int Num = Convert.ToInt32( Gender.Unkown);
+            //explicit casting
+            gender = (Gender)Season.winter;
+
+            short[] Values = (short[])Enum.GetValues(typeof(Gender));
+
+            foreach (short value in Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine($"\n");
+
+            string[] Names = (string[])Enum.GetNames(typeof(Gender));
+            foreach (string name in Names)
+            {
+                Console.WriteLine(name);
+            }
+            
+        }
+    }
+
+    public enum Gender : short
+    {
+        //hidden type is int
+        //it is possible to change the underlying type
+        Unkown = 1,
+        Female = 2,
+        Male = 3
+    }
+
+    public enum Season
+    {
+        winter = 1,
+        spring = 2,
+        summer = 3
+    }
+}
