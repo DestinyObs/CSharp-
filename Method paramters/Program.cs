e754fa1e7cc2aa7cd88399e3035e6c425270da8f
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-04-11 10:04:19 +0100

    new

---
+++ Method paramters/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Method_paramters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ways to make method parameters arrays
+            //method overlaoding 
+            //specify parameter ddefaults
+            //use optionalAttributes 
+
+            AddNum(10, 20, new int[] { 30, 50 });
+
+        }
+        public static void AddNum(int FNum, int SNum)
+        {
+            AddNum(FNum, SNum, null);
+
+        }
+
+
+        public static void AddNum(int FNum, int SNum, params int[] RON)
+        {
+            int result = FNum + SNum;
+            if (RON != null)
+            {
+                foreach (int i in RON)
+                {
+                    result += i;
+
+                }
+
+            }
+            Console.WriteLine("Sum = " + result);
+        }
+    }
+}
